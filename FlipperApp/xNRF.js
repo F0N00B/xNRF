31d0651212d7b1e8e019032891af1b8788aae2c5
F0N00B <f0noob@gmx.com>
2025-05-21 20:51:07 +0300

    Add files via upload

---
+++ FlipperApp/xNRF.js
@@ -0,0 +1,170 @@
+// import modules
+let eventLoop = require("event_loop");
+let gui = require("gui");
+let loadingView = require("gui/loading");
+let submenuView = require("gui/submenu");
+let textBoxView = require("gui/text_box");
+let gpio = require("gpio");
+
+
+let serial = require("serial");
+serial.setup("usart", 115200);
+
+var set = 0;
+var onlines = "0";
+
+
+let res_pin = gpio.get("pc0"); // C0 / 16 PIN - ESP32 Reset PIN
+res_pin.init({ direction: "out", outMode: "push_pull"});
+
+
+res_pin.write(true);
+delay(300);
+let res_pins = gpio.get("pc0"); // C0 / 16 PIN - ESP32 Reset PIN
+res_pins.init({ direction: "in", inMode: "analog" });
+res_pins.write(false);
+delay(300);
+
+function SerialCMD(cmd) {
+		serial.setup("usart", 115200);
+		delay(500);
+		serial.write(cmd);
+		delay(500);
+		serial.end();
+}
+
+print("-------------------");
+print("NRF24 Serial Jammer");
+print("-------------------");
+print("by x01r");
+print("");
+delay(250);
+print("");
+print("Waiting for NRF ..");
+
+delay(500);
+
+
+// check for online NRF's
+while (set === 0) {
+    let rx_data = serial.readln(1000);
+    if (rx_data !== undefined && rx_data === "000" ||  rx_data === "001" ||  rx_data === "011" ||  rx_data === "111" ||  rx_data === "110" ||  rx_data === "100" ||  rx_data === "101" ||  rx_data === "010" ) {
+		
+		if (rx_data === "000") {
+			onlines = "0";
+		} else if (rx_data === "001" || rx_data === "100" || rx_data === "010") {
+			onlines = "1";
+		} else if (rx_data === "011" || rx_data === "110" ||  rx_data === "101") {
+			onlines = "2";
+		} else if (rx_data === "111") {
+			onlines = "3";
+		}
+        print(""+onlines + " NRF's ONLINE");
+		
+		if (onlines !== "0") { // online NRF's detected
+			
+				set++;
+				delay(1000);
+				serial.end();
+				print("Loading Jammer Menu ...");
+				delay(2000);
+			
+		}
+    }
+}
+
+
+// declare view instances
+let views = {
+	
+    WiFi: textBoxView.makeWith({
+        text: "WiFi Mode Active",
+    }),
+    BLE: textBoxView.makeWith({
+        text: "BLE Mode Active",
+    }),
+    Bluetooth: textBoxView.makeWith({
+        text: "Bluetooth Mode Active",
+    }),
+    USB: textBoxView.makeWith({
+        text: "USB Device Mode Active",
+    }),
+    Video: textBoxView.makeWith({
+        text: "Video Stream Mode Active",
+    }),
+    RC: textBoxView.makeWith({
+        text: "Romote Contr. Mode Active",
+    }),
+    Full: textBoxView.makeWith({
+        text: "Full 2.4G Mode Active",
+    }),
+    jammer: submenuView.makeWith({
+        header: "NRF24 Serial Jammer",
+        items: [
+            "WiFi",
+            "BLE",
+            "Bluetooth",
+            "USB Devices",
+            "Video Streamers",
+            "Remote Controllers",
+            "Full 2.4G Jammer",
+            "Exit Jammer",
+        ],
+    }),
+};
+
+
+// jamming mode selection
+eventLoop.subscribe(views.jammer.chosen, function (_sub, index, gui, eventLoop, views) {
+	
+    if (index === 0) {
+        gui.viewDispatcher.switchTo(views.WiFi);
+		SerialCMD("WiFi");
+		
+    } else if (index === 1) {
+        gui.viewDispatcher.switchTo(views.BLE);
+		SerialCMD("BLE");
+		
+    } else if (index === 2) {
+        gui.viewDispatcher.switchTo(views.Bluetooth);
+		SerialCMD("Bluetooth");
+		
+    } else if (index === 3) {
+        gui.viewDispatcher.switchTo(views.USB);
+		SerialCMD("USB");
+		
+    } else if (index === 4) {
+        gui.viewDispatcher.switchTo(views.Video);
+		SerialCMD("Video"
+		
+    } else if (index === 5) {
+        gui.viewDispatcher.switchTo(views.RC);
+		SerialCMD("RC");
+		
+    } else if (index === 6) {
+        gui.viewDispatcher.switchTo(views.Full);
+    } else if (index === 7) {
+        eventLoop.stop();
+		SerialCMD("OFF");
+    }
+}, gui, eventLoop, views);
+
+
+
+
+// jammer main menu
+eventLoop.subscribe(gui.viewDispatcher.navigation, function (_sub, _, gui, views, eventLoop) {
+	SerialCMD("OFF");
+    if (gui.viewDispatcher.currentView === views.jammer) {
+        eventLoop.stop();
+		
+        return;
+    }
+    gui.viewDispatcher.switchTo(views.jammer);
+}, gui, views, eventLoop);
+
+
+
+// run jammer UI
+gui.viewDispatcher.switchTo(views.jammer);
+eventLoop.run();
---
+++ README.md
@@ -0,0 +1,85 @@
+<div align="center">
+
+  # ☢️ xNRF 
+###
+![IMG_2068](https://github.com/user-attachments/assets/3d410abd-1113-4b95-bcba-5ea65bdcd4c3)
+###
+#### 🐬 FlipperZero multiple (x3) NRF24 - ESP32 Serial Jammer 🐬
+##### 🔇 WiFi, BLE, Bluetooth, Video Streamers, Remotly Controlled devices, Full 2.4Ghz spectrum 🔇
+###
+&nbsp;
+###
+</div>
+
+
+## 💡 Hardware used
+<table>
+<tbody>
+<tr><td>x1</td><td>FlipperZero</td></tr>
+<tr><td>x1</td><td>ESP32 Wroom</td></tr>
+<tr><td>x1-3</td><td>NRF24</td></tr>
+</tbody>
+</table>
+
+###
+&nbsp;
+###
+
+## 🛠 Wiring Diagram & Pinouts
+
+![diagram](https://github.com/user-attachments/assets/024bed2b-0849-4aa5-bb4d-82bf2a025141)
+
+
+<table>
+<thead><th>NRF24</th><th>ESP32 (GPIO)</th><th> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </th><th>ESP32 (GPIO)</th><th>Flipper Zero</th></thead>
+<tbody>
+<tr><td>VCC</td><td>3.3V</td><td></td><td>VIN (V5)</td><td>V5 - GPIO 1 </td></tr>
+<tr><td>GND</td><td>GND</td><td></td><td>GND</td><td>GND - GPIO 18 </td></tr>
+<tr><td>SCK</td><td>GPIO 18</td><td></td><td>TX0</td><td>RX - GPIO 14 </td></tr>
+<tr><td>MISO</td><td>GPIO 19</td><td></td><td>RX0</td><td>RX - GPIO 13 </td></tr>
+<tr><td>MOSI</td><td>GPIO 23</td><td></td><td>EN (RST)</td><td>C0 - GPIO 16 </td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+<tr><td>NRF #1</td><td></td></tr>
+<tr><td>CE</td><td>GPIO 5</td></tr>
+<tr><td>CSN</td><td>GPIO 17</td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+<tr><td>NRF #2</td><td></td></tr>
+<tr><td>CE</td><td>GPIO 16</td></tr>
+<tr><td>CSN</td><td>GPIO 4</td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+<tr><td>NRF #3</td><td></td></tr>
+<tr><td>CE</td><td>GPIO 15</td></tr>
+<tr><td>CSN</td><td>GPIO 2</td></tr>
+  
+</tbody>
+</table>
+
+* IRQ is Not connected
+###
+&nbsp;
+###
+
+
+
+## 🚀 Building Steps
+
+##### 1. Build your hardware
+##### 2. Flash your ESP32 using the WebFlasher / from Source.
+##### 3. Download the FlipperZero App (xNRF.js) and place it in "SD Card/apps/Scripts"
+##### 4. Connect your built and firmware flashed device to your Flipper Zero device
+##### 5. Turn v5 ON your FlipperZero GPIO
+##### 6. Start xNRF.js app
+#### 7. Jam the Planet ☠️
+###
+&nbsp;
+###
+###
+&nbsp;
+###
+###
+&nbsp;
+###
+  
+###### made by x01r.  
+###### For educational purposes only, JAMMING IS ILLEGAL!
+###### Do not ask me to implement new functions in this code and do not ask me for help.
